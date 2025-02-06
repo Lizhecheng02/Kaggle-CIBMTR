b7116c7611c6be40a2c97ee599488039d4ce8b32
unknown <2569618533@qq.com>
2025-02-06 11:04:32 -0800

    add kaggle inference part

--- deberta/inference.py
+++ deberta/inference.py
@@ -56,3 +56,32 @@ with torch.no_grad():
     pooled_output = model.pooler(outputs.last_hidden_state)
 print(pooled_output)
 print(pooled_output.shape)
+
+
+# +++++++++++++++++++++ Kaggle Inference +++++++++++++++++++++
+# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# tokenizer = AutoTokenizer.from_pretrained("/kaggle/input/debertav3base")
+# MODEL_PATH = "/kaggle/input/cib-deberta-v3-base-kaplan/deberta-v3-base-kaplan/"
+
+# train_texts = train["text"].to_list()
+
+# models = []
+# for i in range(10):
+#     model_path = MODEL_PATH + f"Fold{i}/checkpoint-2430"
+#     model = AutoModelForSequenceClassification.from_pretrained(model_path).to(DEVICE)
+#     model.eval()
+#     models.append(model)
+
+# mean_features = []
+# with torch.no_grad():
+#     for train_text in tqdm(train_texts, total=len(train_texts)):
+#         features = []
+#         inputs = tokenizer(train_text, return_tensors="pt", padding=True, truncation=True, max_length=520).to(DEVICE)
+#         for model in models:
+#             outputs = model.deberta(**inputs)
+#             pooled_output = model.pooler(outputs.last_hidden_state)
+#             features.append(pooled_output.cpu().numpy())
+#         features = np.mean(features, axis=0)
+#         mean_features.append(features)
+# mean_features = np.vstack(mean_features)
+# +++++++++++++++++++++ Kaggle Inference +++++++++++++++++++++
